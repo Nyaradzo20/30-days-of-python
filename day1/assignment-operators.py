d2837eb7a079f0156d101c782400deab56ef4a6c
Nyaradzo20 <nchinyati20@gmail.com>
2021-12-08 13:23:24 +0000

    assignment operators

---
+++ day1/assignment-operators.py
@@ -0,0 +1,23 @@
+#used to assigOAAn value to a variable...
+
+c = 5
+b = 20
+#subtract and
+b -= c
+print(b)
+#it subracts  the right operand from the left b = b-c
+
+#multiply and
+c *= b
+print(c)
+
+#divide and
+#print(b %= a )
+#exponent and
+#Performs exponential (power) calculation on operators and assign value to the left operand
+c **= b
+print(c)
+#floor division
+#Floor Division could be described as the normal division operation, which when applied returns the largest possible integer, which might be less than or equal to what the normal division operator returns. Floor Division in Python is an operator, and it is . This operator could be used to return both integers as well as floating int
+b //= c
+print(b)
