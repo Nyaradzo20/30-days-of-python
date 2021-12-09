195d5c0c0f70f1d4d4747f407f11b613fa5bdaf2
Nyaradzo20 <nchinyati20@gmail.com>
2021-12-09 11:56:04 +0000

    clean code

---
+++ day2/if-else-statements.py
@@ -0,0 +1,9 @@
+age = 22
+if age > 18:
+    print("eligible")
+else:
+    ("not eligible, sorry")
+#better method ********************************************
+age = 16 
+message = "eligible" if age >= 18 else "not eligible"
+print(message)
