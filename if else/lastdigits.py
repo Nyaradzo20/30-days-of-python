b3161690787e9d06dd1561255d12c4c7e0f50520
Nyaradzo <nchinyati20@gmail.com>
2021-12-21 08:49:29 +0000

    lastdig

---
+++ if else/lastdigits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+number = -9009
+if number < 0:
+    last_int = number % -10
+else:
+    last_int = number % 10
+if last_int > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, last_int))
+elif last_int < 6 and last_int != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, last_int))
+else:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_int))
