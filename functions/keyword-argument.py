44b3eacf0398852c7bfdd43c62b5b1900fb7b93c
Nyaradzo <nchinyati20@gmail.com>
2021-12-23 10:31:27 +0000

    keyword

--- functions/keyword-argument.py
+++ functions/keyword-argument.py
@@ -3,4 +3,6 @@ def increment(number, by):
 #another method
 #result = increment(6, 9)
 #print(result)
-print(increment(6, 9))
+#print(increment(6, 9))
+print(increment(6, by = 9))
+#by is  a keyword in this scenario and it makes the code readable
