2b7a67ae94a96dab5d780a5630f44ab3bda5a2c5
Nyaradzo <nchinyati20@gmail.com>
2021-12-23 09:48:14 +0000

    function returning value

---
+++ functions/return.py
@@ -0,0 +1,6 @@
+#using a return statement to get a value from function
+def get_greeting(name):
+    return f"hello {name}" 
+message = get_greeting("nyaradzo")
+print(message)
+#all functions return none unless you specifically return a value
