269a95cf8f3f2594132403a3cc397e4596ef99ef
Nyaradzo <nchinyati20@gmail.com>
2021-12-23 09:32:35 +0000

    re-using function

--- functions/create.py
+++
@@ -1,6 +0,0 @@
-def greet():#parameters should be passed between the brackets
-    print('hi nyari')
-    print('welcome aboard')
-greet()#arguments to be passed between the brackets
-#parameter is the input you need for your function
-#arguments is the actual value of parameters
---
+++ functions/create2.py
@@ -0,0 +1,14 @@
+#def greet():#parameters should be passed between the brackets
+def greet(name):
+    # print('hi ')
+    print(f"Hi {name}")
+    print('welcome aboard')
+greet("Nyaradzo")#arguments to be passed between the brackets
+#parameter is the input you need for your function
+#arguments is the actual value of parameters
+greet("Nomatter")
+greet("Awet")
+greet("beauty")
+greet("Munashe")
+greet("Mirriam")
+greet("Baron")#see a function is reusable 
