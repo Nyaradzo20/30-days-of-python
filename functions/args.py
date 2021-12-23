74ff84e43623274133747b3caf181e798f39a967
Nyaradzo <nchinyati20@gmail.com>
2021-12-23 14:42:30 +0000

    variables in functions

--- functions/args.py
+++ functions/args.py
@@ -2,4 +2,5 @@ def user(**user):
        print(user)
 
 user(id=1, name="john", age=33)
-#returns a dictionary type of data structure
+#returns a dictionary type of data structure because of thr asterics
+#python auto packs them into a dict
---
+++ functions/scope.py
@@ -0,0 +1,14 @@
+message = 'n'
+def greet(name):
+    message = 'a'
+greet("Nyaradzo")    
+print(message)
+
+
+#p;rint should be isdented  mufunction
+#its a local variable
+#after execution its garbaged
+
+#however if its outside(before)of the function it is a global variable
+#the stay in memory for  a  long time but its not a dvisible  to use them because they can ruin your code
+#n will be printed  because of thr outer variable wihich is a global variable
